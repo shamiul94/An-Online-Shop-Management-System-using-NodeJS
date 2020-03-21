9595e03a651c64b30fa6e35c3914bacdd1ee68ba
Shamiul Hasan <1505038.sh@ugrad.cse.buet.ac.bd>
2020-03-22 03:12:45 +0600

    Main codes start from week 3.

---
+++ 3. Understanding the Basics/play.js
@@ -0,0 +1,37 @@
+// // const a = 26;
+// // console.log(a);
+// //
+// // var j = true;
+// // console.log(j);
+// //
+// // function f(a, b) {
+// //     var x = a * b;
+// //     return a + b;
+// // }
+// //
+// // // console.log('x is: ' + x);
+// //
+// // // var k = f(2, 3);
+// // // console.log(k);
+// //
+// //
+// // const v = (a, b) => (a + b);
+// //
+// //
+// // console.log(v(2, 3));
+//
+//
+// const person = {
+//     name: 'al',
+//     roll: 23
+// };
+//
+// console.log(person.name);
+//
+// const name = ['adf', 'asdfd'];
+//
+// // for (let h of name) {
+// //     console.log(h);
+// // }
+//
+// console.log(name.map(test => ))
