125282f1f3a2908d30f7aa161d03fb2c772f81a4
Shamiul Hasan <1505038.sh@ugrad.cse.buet.ac.bd>
2020-03-23 02:24:21 +0600

    redirecting a page

--- 3. Understanding the Basics/app.js
+++ 3. Understanding the Basics/app.js
@@ -1,9 +1,50 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     // console.log(req.url, req.method, req.headers);
-    console.log(req.url);
-    process.exit();
+    // console.log(req.url);
+    // console.log(res);
+    // res.write();
+    // process.exit();
+
+    // res.setHeader('Content-Type', 'text/html');
+    // res.write('<html>');
+    // res.write('<head><title> Node JS response html! Yayy! </title></head>');
+    // res.write('<body> Node JS response html! Yayy! </body>');
+    // res.write('</html>');
+    // res.end();
+
+
+    const url = req.url;
+    const method = req.method;
+    console.log(method);
+    // const data = req.
+
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title> Node JS response html! Yayy! </title></head>');
+        res.write('<body><form action="/message" method="post"><input type="text" name="msg"><button type="submit">' +
+            'send </button> </form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('submitted.txt', 'Test');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        // console.log('hi');
+        return res.end();
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> Node JS response html! Yayy! </title></head>');
+    res.write('<body> Node JS response html! Yayy! </body>');
+    res.write('</html>');
+    res.end();
+
 });
 
 server.listen(3000);
