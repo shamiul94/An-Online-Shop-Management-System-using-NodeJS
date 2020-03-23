bb85ec234b871cdb8241b9f600fee7d1632f9561
Shamiul Hasan <1505038.sh@ugrad.cse.buet.ac.bd>
2020-03-24 01:35:43 +0600

    modularization.

--- 3. Understanding the Basics/app.js
+++ 3. Understanding the Basics/app.js
@@ -1,70 +1,8 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    // console.log(req.url, req.method, req.headers);
-    // console.log(req.url);
-    // console.log(res);
-    // res.write();
-    // process.exit();
-
-    // res.setHeader('Content-Type', 'text/html');
-    // res.write('<html>');
-    // res.write('<head><title> Node JS response html! Yayy! </title></head>');
-    // res.write('<body> Node JS response html! Yayy! </body>');
-    // res.write('</html>');
-    // res.end();
-
-
-    const url = req.url;
-    const method = req.method;
-    // console.log(method);
-    // const data = req.
-
-    if (url === '/') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title> Node JS response html! Yayy! </title></head>');
-        res.write('<body><form action="/message" method="post"><input type="text" name="msg"><button type="submit">' +
-            'send </button> </form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if (url === '/message' && method === 'POST') {
-        /*
-        Implementing extraction of data from post request.
-         */
-
-        const body = [];
-        req.on('data', chunk => {
-            console.log(chunk + ',');
-            body.push(chunk);
-        });
-
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const dataOnly = parsedBody.split('=')[1];
-
-            /*
-                writeFileSync ==> blocking code
-                writeFile ==> non-blocking code
-             */
-            // fs.writeFileSync('submitted.txt', dataOnly);
-            fs.writeFile('submitted.txt', dataOnly, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title> Node JS response html! Yayy! </title></head>');
-    res.write('<body> Node JS response html! Yayy! </body>');
-    res.write('</html>');
-    res.end();
-});
+// const server = http.createServer(routes);
+console.log(routes.text);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
---
+++ 3. Understanding the Basics/routes.js
@@ -0,0 +1,73 @@
+const fs = require('fs');
+
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title> Node JS response html! Yayy! </title></head>');
+        res.write('<body><form action="/message" method="post"><input type="text" name="msg"><button type="submit">' +
+            'send </button> </form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        /*
+        Implementing extraction of data from post request.
+         */
+
+        const body = [];
+        req.on('data', chunk => {
+            console.log(chunk + ',');
+            body.push(chunk);
+        });
+
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const dataOnly = parsedBody.split('=')[1];
+
+            /*
+                writeFileSync ==> blocking code
+                writeFile ==> non-blocking code
+             */
+            // fs.writeFileSync('submitted.txt', dataOnly);
+            fs.writeFile('submitted.txt', dataOnly, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
+    }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title> Node JS response html! Yayy! </title></head>');
+    res.write('<body> Node JS response html! Yayy! </body>');
+    res.write('</html>');
+    res.end();
+};
+
+
+/*
+First way of exporting module AKA connecting routes.js with app.js
+ */
+
+// way-1
+// module.exports = requestHandler;
+
+// way-2
+// module.exports = {
+//     handler: requestHandler,
+//     text: 'some text'
+// };
+
+// way - 3
+// module.exports.handler = requestHandler;
+// module.exports.text = 'some text';
+
+// way-4 -> works only for node js (not for raw js).
+// exports.handler = requestHandler;
+// exports.text = 'some text';
