ca628b79d2f06533c269190d0825e3c30ea4aa4b
Shamiul Hasan <1505038.sh@ugrad.cse.buet.ac.bd>
2020-03-25 04:43:32 +0600

    Adding dummy middlewares.

--- 5. Working with Express.js/app.js
+++ 5. Working with Express.js/app.js
@@ -1,5 +1,24 @@
 const http = require('http');
+const express = require('express');
 
-const server = http.createServer();
+const app = express();
 
+app.use((req, res, next) => {
+    console.log("In the middleware.");
+    next();
+});
+
+
+app.use((req, res, next) => {
+    console.log("In another middleware.");
+    // instead of setheader() and write() functions we use send()
+    res.send('<h1>Another Middleware.</h1>')
+});
+/*
+const server = http.createServer(app);
 server.listen(3000);
+--> this chunk of code can be replaced by app.listen()
+--> this function actually does those things under the hood.
+ */
+
+app.listen(3000);
